4c70d6d645c83a7e68a07a5132c597dc6963b789
franmolinaadan <franmolinaadan@outlook.es>
2023-03-28 15:57:37 +0200

    Refactorizado.

--- src/Coche.java
+++ src/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_FRAN_MOLINA(int aceleracion) {
         velocidad+=aceleracion;
     }
 
-    public void decelerar(int decelerar) {
+    public void decelerar_FRAN_MOLINA(int decelerar) {
         velocidad-=decelerar;
         if (velocidad<0)velocidad=0;
     }
--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -3,28 +3,28 @@ import org.junit.jupiter.api.Test;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero_FRAN_MOLINA(){
         Coche nuevoCoche =new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta_FRAN_MOLINA(){
         Coche nuevoCoche =new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerar_FRAN_MOLINA(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_disminuye(){
+    public void test_al_decelerar_un_coche_su_velocidad_disminuye_FRAN_MOLINA(){
         Coche nuevoCoche =new Coche();
         nuevoCoche.velocidad=50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerar_FRAN_MOLINA(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
     @Test
-    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+    public void test_al_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_FRAN_MOLINA(){
         Coche nuevoCoche =new Coche();
         nuevoCoche.velocidad=50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerar_FRAN_MOLINA(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 }
