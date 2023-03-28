164e3e5559024098a91664e991d5d45cf338b5d9
franmolinaadan <franmolinaadan@outlook.es>
2023-03-28 15:47:07 +0200

    Añadido acelerar, decelerar y comprobacion.

--- src/Coche.java
+++ src/Coche.java
@@ -1,3 +1,12 @@
 public class Coche {
     public int velocidad;
+
+    public void acelerar(int aceleracion) {
+        velocidad+=aceleracion;
+    }
+
+    public void decelerar(int decelerar) {
+        velocidad-=decelerar;
+        if (velocidad<0)velocidad=0;
+    }
 }
